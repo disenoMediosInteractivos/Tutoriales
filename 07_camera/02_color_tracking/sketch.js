cb786ca9f860d3d431ff8976f009a8a4ad5e8bff
laurajunco <lalaj31@hotmail.com>
2018-09-16 18:06:07 -0500

    Keep working on example 2

--- 07_camera/02_color_tracking/sketch.js
+++ 07_camera/02_color_tracking/sketch.js
@@ -4,6 +4,11 @@ var tracker;
 var w = 640;
 var h = 480;
 
+var rojo;
+var verde;
+var azul;
+var rango = 32;
+
 var captureArgs = {
   audio: false,
   video: {
@@ -12,6 +17,11 @@ var captureArgs = {
   }
 }
 
+function obtenerColor(r, g, b) {
+  rojo = r;
+  verde = g;
+  azul = b;
+}
 function setup() {
     capture = createCapture(captureArgs, function() {
         console.log('captura lista :)')
@@ -23,6 +33,34 @@ function setup() {
     capture.parent('container');
     canvas.parent('container');
     // capture.hide();
+
+    obtenerColor(255, 255, 255);
+    tracking.ColorTracker.registerColor('seleccion', function(r, g, b) {
+    if (r >= rojo - rango && r <= rojo + rango &&
+        g >= verde - rango && g <= verde + rango &&
+        b >= azul - rango && b <= azul + rango) {
+      return true;
+    }
+    return false;
+  });
+
+  tracker = new tracking.ColorTracker(['seleccion']);
+  tracker.minDimension = 20;
+  capture.elt.id = 'p5video';
+
+  tracking.track('#p5video', tracker, {
+       camera: true
+   });
+
+  tracker.on('track', function (event) {
+        clear();
+        strokeWeight(2);
+        stroke(255, 0, 0);
+        noFill();
+        event.data.forEach(function (r) {
+            rect(r.x, r.y, r.width, r.height);
+        })
+  });
 }
 
 function draw() {
