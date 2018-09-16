9c9d6c82232c1b69f40610b715ed5676c20fe3ee
laurajunco <lalaj31@hotmail.com>
2018-09-16 15:32:40 -0500

    Start to add setup empty example º

---
+++ 07_camera/01_setup/sketch.js
@@ -0,0 +1,26 @@
+var capture;
+var w = 640;
+var h = 480;
+
+function setup() {
+    capture = createCapture({
+        audio: false,
+        video: {
+            width: w,
+            height: h
+        }
+    }, function() {
+        console.log('capture ready.')
+    });
+    capture.elt.setAttribute('playsinline', '');
+    capture.size(w, h);
+    createCanvas(w, h);
+    capture.hide();
+}
+
+function draw() {
+    image(capture, 0, 0, w, h);
+    capture.loadPixels();
+    if (capture.pixels.length > 0) {
+    }
+}
