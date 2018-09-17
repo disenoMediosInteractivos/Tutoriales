726039f19959a5476acf2c1ebbdc1c1a1fb8f0a6
laurajunco <lc.junco10@uniandes.edu.co>
2018-09-17 16:38:45 -0500

    Finish base for example 2

--- 07_camera/05_expression_detection/sketch.js
+++ 07_camera/05_expression_detection/sketch.js
@@ -24,16 +24,32 @@ function setup() {
     tracker = new clm.tracker();
     tracker.init(pModel);
     tracker.start(capture.elt);
+
+
 }
 
 function draw() {
-    image(capture, 0, 0, w, h);
+  image(capture, 0, 0, w, h);
+  var positions = tracker.getCurrentPosition();
 
-    // noFill();
-    // stroke(255);
-    // beginShape();
-    // for (var i = 0; i < positions.length; i++) {
-    //     vertex(positions[i][0], positions[i][1]);
-    // }
-    // endShape();
+  noFill();
+  stroke(0, 255, 0);
+
+  if (positions.length > 0) {
+
+    //dibujar lineas
+    beginShape();
+    for (var i = 0; i < positions.length; i++) {
+        vertex(positions[i][0], positions[i][1]);
+    }
+    endShape();
+
+    //numeros
+    noStroke();
+    for (var i = 0; i < positions.length; i++) {
+        fill(255, 0, 0);
+        ellipse(positions[i][0], positions[i][1], 4, 4);
+        // text(i, positions[i][0], positions[i][1]);
+    }
+  }
 }
