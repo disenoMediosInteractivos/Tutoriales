3c0bf9ad3ab7dd2d0edc09e0c2f4b9d121c7fa3f
laurajunco <lc.junco10@uniandes.edu.co>
2018-09-16 21:49:52 -0500

    Finish example 2

--- 07_camera/02_color_tracking/sketch.js
+++ 07_camera/02_color_tracking/sketch.js
@@ -7,7 +7,8 @@ var h = 480;
 var rojo;
 var verde;
 var azul;
-var rango = 32;
+var col;
+var rango = 25;
 
 var captureArgs = {
   audio: false,
@@ -21,52 +22,56 @@ function obtenerColor(r, g, b) {
   rojo = r;
   verde = g;
   azul = b;
+  col = color(rojo, verde, azul);
 }
+
 function setup() {
     capture = createCapture(captureArgs, function() {
         console.log('captura lista :)')
     });
 
     capture.size(w, h);
-    canvas = createCanvas(w, h);
+    pixelDensity(1);
+    canvas= createCanvas(w, h);
 
     capture.parent('container');
     canvas.parent('container');
-    // capture.hide();
 
     obtenerColor(255, 255, 255);
     tracking.ColorTracker.registerColor('seleccion', function(r, g, b) {
     if (r >= rojo - rango && r <= rojo + rango &&
         g >= verde - rango && g <= verde + rango &&
         b >= azul - rango && b <= azul + rango) {
-      return true;
-    }
+        return true;
+      }
     return false;
   });
 
-  tracker = new tracking.ColorTracker(['seleccion']);
-  tracker.minDimension = 20;
-  capture.elt.id = 'p5video';
+      tracker = new tracking.ColorTracker(['seleccion']);
+      tracker.minDimension = 20;
+      capture.elt.id = 'p5video';
 
-  tracking.track('#p5video', tracker, {
-       camera: true
-   });
+      tracking.track('#p5video', tracker, {
+           camera: true
+       });
 
-  tracker.on('track', function (event) {
+      tracker.on('track', function (event) {
         clear();
         strokeWeight(2);
-        stroke(255, 0, 0);
+        stroke(col);
         noFill();
         event.data.forEach(function (r) {
             rect(r.x, r.y, r.width, r.height);
         })
-  });
-}
+      });
+    }
 
-function draw() {
-    image(capture, 0, 0, w, h);
-    capture.loadPixels();
+function mousePressed() {
+    if (mouseX > 0 && mouseX < width &&
+        mouseY > 0 && mouseY < height) {
 
-    if (capture.pixels.length > 0) {
+          capture.loadPixels();
+          var sel = capture.get(mouseX, mouseY);
+          obtenerColor(sel[0], sel[1], sel[2]);
     }
 }
